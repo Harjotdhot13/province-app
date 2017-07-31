719949a1d1ca6ca2f1a47367d304a59ee7587034
harjotdhot <harjotdhot91@gmail.com>
2017-07-31 15:07:31 -0400

    update province-detail activator

--- src/app/provinces/province-detail-activator.service.ts
+++ src/app/provinces/province-detail-activator.service.ts
@@ -5,14 +5,14 @@ import { ProvinceService } from "../shared/province.service";
 
 @Injectable()
 export class ProvinceDetailActivator implements CanActivate {
-  constructor(private provinceService: ProvinceService, private router:Router) {}
+  constructor(private provinceService: ProvinceService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
-   const provinceExists = !!this.provinceService.getProvince(route.params['provinceCode']);
-
-   if(!provinceExists) {
-      this.router.navigate(['/404'])
-    }
-    return provinceExists;
+    this.provinceService.getProvince(route.params['provinceCode']).subscribe(provinceExists => {
+      if (!provinceExists) {
+        this.router.navigate(['/404']);
+      }
+    });
+    return true;
   }
 }
